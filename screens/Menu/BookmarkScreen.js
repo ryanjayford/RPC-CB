1a856d9ee70618e90ba9f3343b4eef9f713e5208
khristler <royceshe_12@yahoo.com>
2022-03-09 16:10:21 +0800

    updated the design of the details page

--- screens/Menu/BookmarkScreen.js
+++ screens/Menu/BookmarkScreen.js
@@ -1,48 +1,54 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity,Linking } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity,Linking,ScrollView } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 import * as WebBrowser from 'expo-web-browser';
+import { useTheme } from '@react-navigation/native';
 
 const BookmarkScreen = () => {
-
+    const { colors } = useTheme();
     const Ebg = async () => {
         let webLink = 'https://www.ebgsystems.com/';
         let result = await WebBrowser.openBrowserAsync(webLink);
         //setresult(thisresult = result) 
     };  
+
     return (
       <LinearGradient 
-        colors={['#eeecc5','#fcfdf8']}
+        colors={[colors.linearlight,colors.linearDark]}
         style = {styles.container}
       >
-        <View style={styles.header}>
-          <Text style = {styles.Title}>Office</Text>
-        </View>
-        <View style={{padding: 20}}>
-          <Text style = {styles.SubTitle}>EBG Systems, Inc.</Text>
-          <Text style = {styles.info}>5320 N Sheridan Rd{"\n"}
-                Unit 1610{"\n"}
-                Chicago, IL 60640</Text>
-          <Text style = {styles.info}>P: (773) 866-2088</Text>
-          <Text style = {styles.info}>F: (773) 866-2012 </Text>
-          <TouchableOpacity onPress={()=> Linking.openURL('mailto:info@ebgsystems.com')}>
-            <Text style={styles.link}>info@ebgsystems.com</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=> Ebg()}>
-            <Text style={styles.link}>www.ebgsystems.com</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.header}>
-          <Text style = {styles.Title}>Support</Text>
-        </View>
-        <View style={{padding: 20}}>
-          <Text style = {styles.SubTitle}>Support Team</Text>
-          <Text style = {styles.info}>P: (773) 866-2087</Text>
-          <Text style = {styles.info}>F: (773) 866-2012</Text>
-          <TouchableOpacity onPress={()=> Linking.openURL('mailto:support@ebgsystems.com')}>
-            <Text style={styles.link}>support@ebgsystems.com</Text>
-          </TouchableOpacity>
-        </View>
+        <ScrollView>
+          <View style={[styles.header,{backgroundColor: colors.icon}]}>
+            <Text style = {styles.Title}>Office</Text>
+          </View>
+
+          <View style={{padding: 20, margin: 20, borderRadius: 5, backgroundColor: colors.background}}>
+            <Text style = {[styles.SubTitle, {color: colors.textGreen}]}>EBG Systems, Inc.</Text>
+            <Text style = {[styles.info, {color: colors.textLight}]}>5320 N Sheridan Rd{"\n"}
+                  Unit 1610{"\n"}
+                  Chicago, IL 60640</Text>
+            <Text style = {[styles.info, {color: colors.textLight}]}>P: (773) 866-2088</Text>
+            <Text style = {[styles.info, {color: colors.textLight}]}>F: (773) 866-2012 </Text>
+            <TouchableOpacity onPress={()=> Linking.openURL('mailto:info@ebgsystems.com')}>
+              <Text style={[styles.link, {color: colors.email}]}>info@ebgsystems.com</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=> Ebg()}>
+              <Text style={[styles.link, {color: colors.email}]}>www.ebgsystems.com</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.header,{backgroundColor: colors.icon}]}>
+            <Text style = {styles.Title}>Support</Text>
+          </View>
+          <View style={{padding: 20, margin: 20, borderRadius: 5, backgroundColor: colors.background}}>
+            <Text style = {[styles.SubTitle, {color: colors.textGreen}]}>Support Team</Text>
+            <Text style = {[styles.info, {color: colors.textLight}]}>P: (773) 866-2087</Text>
+            <Text style = {[styles.info, {color: colors.textLight}]}>F: (773) 866-2012</Text>
+            <TouchableOpacity onPress={()=> Linking.openURL('mailto:support@ebgsystems.com')}>
+              <Text style={[styles.link, {color: colors.email}]}>support@ebgsystems.com</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </LinearGradient>
     );
 };
@@ -56,7 +62,7 @@ const styles = StyleSheet.create({
         //justifyContent: 'center',
     },
     header: {
-      backgroundColor: '#008000',
+      //backgroundColor: '#008000',
       padding: 10
     },
     Title: {
@@ -66,7 +72,7 @@ const styles = StyleSheet.create({
     },
     SubTitle: {
       fontSize: 28,
-      color: '#008000',
+      //color: '#008000',
       marginBottom: 10,
     },
     info: {
@@ -76,7 +82,7 @@ const styles = StyleSheet.create({
       marginTop: 10,
       fontSize: 18,
       fontWeight: 'bold',
-      color: '#008000',
+      //color: '#008000',
       textDecorationLine: 'underline',
 
     },
